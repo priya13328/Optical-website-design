246c20cc94afc58af1eb003b4014ac26a6f9cab6
priya <13328priya@gmail.com>
2021-10-12 21:47:57 +0530

    Optical website design

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById("mobile-cross").stytle.left="0px";
+
+}
+function closeMenu(){
+    document.getElementById("mobile-bar").stytle.left="-280px";
+
+}
